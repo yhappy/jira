5f2ef8ad9b5d86a7e6ea03295d06be3380e3cc9c
yhappy <yhappy1992@gmail.com>
2022-11-15 02:19:34 +0800

    chore: 添加 TS 类型

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { cleanObject, useDebounce, useMount } from "utils/index";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const deouncedParam = useDebounce(param, 200);
+
+  const [users, setUsers] = useState([]);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(deouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [deouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((keys) => {
+    // @ts-ignore
     const value = result[keys];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[keys];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceedValue, setDebounceedValue] = useState(value);
 
   useEffect(() => {
