bcb4a90bd8672fa5b6c4a058f1a515a432a986e1
yhappy <yhappy1992@gmail.com>
2022-11-15 01:49:17 +0800

    feat: 添加 useMount useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import qs from "qs"
-import { cleanObject } from 'utils/index'
+import { cleanObject, useDebounce, useMount } from 'utils/index'
 import { useEffect, useState } from "react"
 import { SearchPanel } from "./search-panel"
 import { List } from "./list"
@@ -11,26 +11,27 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+  const deouncedParam = useDebounce(param, 2000)
+
   const [users, setUsers] = useState([])
 
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(deouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [deouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
-
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,26 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceedValue, setDebounceedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounceedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debounceedValue;
 };
