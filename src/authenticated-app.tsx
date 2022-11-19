d928e223cdbc6dc4585cf3f24410ba90ed4fe4fc
yhappy <yhappy1992@gmail.com>
2022-11-20 03:39:49 +0800

    style: 优化项目列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,51 @@
+import styled from "@emotion/styled";
+import { Row } from "components/libs";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
-
+import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
+import { Dropdown, Menu } from "antd";
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>logout</button>
-      <ProjectListScreen />
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <SoftwareLogo width={"18rem"} color={"rgb(38,132,255)"} />
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Dropdown
+            overlay={
+              <Menu>
+                <Menu.Item key={"logout"}>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a onClick={logout}>登出</a>
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a onClick={(e) => e.preventDefault()}>Hi, {user?.name}</a>
+          </Dropdown>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+
+const Header = styled(Row)`
+  padding: 3.2rem;
+  height: 6rem;
+  grid-area: header;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { cleanObject, useDebounce, useMount } from "utils/index";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useHttp } from "utils/http";
+import styled from "@emotion/styled";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -24,9 +25,14 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
